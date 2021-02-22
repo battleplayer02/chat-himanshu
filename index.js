f72bd73498517e0c7efe2ee156c9ea5af3ec8fab
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-02-22 19:48:51 +0530

    a

--- index.js
+++ index.js
@@ -71,7 +71,8 @@ io.on('connection', (socket) => {
 
   //message
   socket.on('code-share-message', function (data) {
-    io.sockets.emit('code-share-message', data);
+    socket.emit('code-share-message', data);
+    socket.broadcast.emit('code-share-message', data);
   });
 
 
