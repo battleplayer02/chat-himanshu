d564166f453077e079dcc3835c8a481a1f82da71
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-05-26 00:59:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 
 // CORS HEADERS MIDDLEWARE
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE"
